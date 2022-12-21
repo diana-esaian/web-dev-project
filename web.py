e14bc83e77fd0125d8876caaebcc234ba91003a5
Diana Yesaian <diana.esaian@gmail.com>
2022-12-21 22:33:27 +0400

    Added templates and static files

--- web.py
+++ web.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return "Hello, world!"
+	return render_template("index.html.j2")
+
+@app.route("/about")
+def about():
+	return render_template("about.html.j2")
+
+@app.route("/article")
+def article():
+	return render_template("article.html.j2")
+
+@app.route("/data")
+def data():
+	return render_template("data.html.j2")
+
 
 if __name__ == "__main__":
 	app.run(debug=True, host="0.0.0.0")
